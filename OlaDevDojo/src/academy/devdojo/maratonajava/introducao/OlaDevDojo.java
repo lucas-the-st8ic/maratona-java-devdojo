0dabeff155a4324d8247545fa0a1272af40ba1b2
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-01-07 16:14:51 -0300

    updates

--- OlaDevDojo/src/academy/devdojo/maratonajava/introducao/OlaDevDojo.java
+++ OlaDevDojo/src/academy/devdojo/maratonajava/introducao/OlaDevDojo.java
@@ -1,8 +1,19 @@
 package academy.devdojo.maratonajava.introducao;
 
+/**
+ * Comentário JavaDoc
+ *
+ */
 public class OlaDevDojo {
-    static void main(String[] args) {
+    //Isso é um comentário de uma linha
 
+    /*Esse é um comentário de diversas linhas ipsum lorem dolus
+    *
+    *
+    *  eratimc impsumlorem eratomc*/
+
+    static void main(String[] args) {
         System.out.println("KA-ME-HA-ME-HA");
+
     }
 }
