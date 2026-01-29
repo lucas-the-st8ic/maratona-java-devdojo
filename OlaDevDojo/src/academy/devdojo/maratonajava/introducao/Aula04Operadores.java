1862b77066eec222eff836f539249864269a9e29
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-01-29 15:54:14 -0300

    updates

--- OlaDevDojo/src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
+++ OlaDevDojo/src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
@@ -7,14 +7,35 @@ public class Aula04Operadores {
             -
             /
             *
+            %
             */
-
-
 
         int numero01 = 10;
         int numero02 = 20;
         double resultado = (double) numero01 / numero02;
+        int resto = 7 % 2;
 
+        System.out.println(resto);
         System.out.println(resultado);
+
+        /*Operadores Lógicos
+        <
+        >
+        ==
+        <=
+        >=
+        !=
+
+        */
+
+        boolean isDezMaiorQueVinte = 10 > 20;
+        boolean isDezMenorQueVinte = 10< 20;
+        boolean isDezIgualAVinte = 10 == 20;
+        boolean isDezDiferenteDeVinte = 10!= 20;
+
+        System.out.println(isDezMaiorQueVinte);
+        System.out.println(isDezMenorQueVinte);
+        System.out.println(isDezIgualAVinte);
+        System.out.println(isDezDiferenteDeVinte);
     }
 }
