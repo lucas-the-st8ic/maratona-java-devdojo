795cc46f21894d834fc9b1cb198da875e5539d40
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-01-29 15:41:25 -0300

    updates

---
+++ OlaDevDojo/src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
@@ -0,0 +1,20 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula04Operadores {
+    static void main(String[] args) {
+        /*Operadores Matemáticos
+            +
+            -
+            /
+            *
+            */
+
+
+
+        int numero01 = 10;
+        int numero02 = 20;
+        double resultado = (double) numero01 / numero02;
+
+        System.out.println(resultado);
+    }
+}
