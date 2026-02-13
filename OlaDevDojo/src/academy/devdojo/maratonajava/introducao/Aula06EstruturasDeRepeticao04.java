2b512523c136ceb3f2631f15a1c885f64164b398
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-02-13 18:57:00 -0300

    updates

---
+++ OlaDevDojo/src/academy/devdojo/maratonajava/introducao/Aula06EstruturasDeRepeticao04.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula06EstruturasDeRepeticao04 {
+    static void main(String[] args) {
+        /*  Dado o valor de um carro,
+        descubra em quantas vezes ele pode
+        ser parcelado.
+
+        */
+    }
+}
