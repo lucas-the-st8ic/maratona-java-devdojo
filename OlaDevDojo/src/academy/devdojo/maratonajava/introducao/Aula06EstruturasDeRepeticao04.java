8d5ee4c7a4dc84f6f3e32d377e9bad614657761d
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-02-16 16:32:42 -0300

    updates

--- OlaDevDojo/src/academy/devdojo/maratonajava/introducao/Aula06EstruturasDeRepeticao04.java
+++ OlaDevDojo/src/academy/devdojo/maratonajava/introducao/Aula06EstruturasDeRepeticao04.java
@@ -1,11 +1,30 @@
 package academy.devdojo.maratonajava.introducao;
 
+import java.util.Scanner;
+
 public class Aula06EstruturasDeRepeticao04 {
     static void main(String[] args) {
         /*  Dado o valor de um carro,
         descubra em quantas vezes ele pode
         ser parcelado.
-
         */
+
+        Scanner input = new Scanner(System.in);
+
+        double valorDoCarro;
+
+        System.out.print("Insira o valor do carro: R$");
+        valorDoCarro = input.nextDouble();
+
+        for ( int parcela = 1; parcela <= valorDoCarro; parcela++ ) {
+            double valorParcela = valorDoCarro / parcela;
+            if(valorParcela < 1000){
+                break;
+            }
+            System.out.println("Parcela: " + parcela + " R$ " + valorParcela);
+        }
+
+
+
     }
 }
