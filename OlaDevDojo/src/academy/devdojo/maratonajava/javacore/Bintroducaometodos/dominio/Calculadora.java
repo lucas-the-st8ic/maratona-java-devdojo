73eb14b42b33411744ca2c2febb9dab6b6f493f3
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-03-15 17:45:32 -0300

    updates

---
+++ OlaDevDojo/src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/dominio/Calculadora.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio;
+
+public class Calculadora {
+
+    public void somaDoisNumeros () {
+        System.out.println(10 + 10);
+    }
+
+    public void subtraiDoisNumeros () {
+        System.out.println(20 - 10);
+    }
+
+    public void multiplicaDoisNumeros (int primeiroNumero, int segundoNumero) {
+        System.out.println(primeiroNumero * segundoNumero);
+    }
+}
---
+++ OlaDevDojo/src/academy/devdojo/maratonajava/javacore/Bintroducaometodos/test/CalculadoraTest01.java
@@ -0,0 +1,13 @@
+package academy.devdojo.maratonajava.javacore.Bintroducaometodos.test;
+
+import academy.devdojo.maratonajava.javacore.Bintroducaometodos.dominio.Calculadora;
+
+public class CalculadoraTest01 {
+    static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+
+        calculadora.somaDoisNumeros();
+        System.out.println("--------------");
+        calculadora.subtraiDoisNumeros();
+    }
+}
