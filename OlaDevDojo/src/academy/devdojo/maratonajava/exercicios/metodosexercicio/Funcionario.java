29de960b1876bbe6eac530d7b9b7b18cfe3f8980
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-03-21 14:14:19 -0300

    updates

---
+++ OlaDevDojo/src/academy/devdojo/maratonajava/exercicios/metodosexercicio/Funcionario.java
@@ -0,0 +1,28 @@
+package academy.devdojo.maratonajava.exercicios.metodosexercicio;
+
+import java.util.Arrays;
+
+public class Funcionario {
+
+    public String nome;
+    public int idade;
+    public double[] salarios;
+
+    public void imprimeDados (){
+        System.out.println("Nome: "+this.nome);
+        System.out.println("Idade: "+this.idade);
+        for(double sal : salarios ) {
+            System.out.println("Salario: R$"+sal);
+        };
+        mediaSalarial();
+    }
+
+    public void mediaSalarial() {
+        double media = Arrays.stream(salarios)
+                .summaryStatistics().getAverage();
+
+        System.out.printf("Média salarial: R$ %.2f \n", media);
+        System.out.println("==============================");
+    }
+}
+
---
+++ OlaDevDojo/src/academy/devdojo/maratonajava/exercicios/metodosexercicio/teste/Main.java
@@ -0,0 +1,26 @@
+package academy.devdojo.maratonajava.exercicios.metodosexercicio.teste;
+
+import academy.devdojo.maratonajava.exercicios.metodosexercicio.Funcionario;
+
+public class Main {
+    static void main(String[] args) {
+        Funcionario funcionario01 = new Funcionario();
+        Funcionario funcionario02 = new Funcionario();
+
+        funcionario01.nome = "Maria";
+        funcionario01.idade = 20;
+        funcionario01.salarios =  new double[] {6541, 7534, 4384};
+
+
+        funcionario01.imprimeDados();
+
+
+
+        funcionario02.nome = "Carlos";
+        funcionario02.idade = 30;
+        funcionario02.salarios =  new double[] {1200, 3458, 7920};
+
+        funcionario02.imprimeDados();
+       
+    }
+}
