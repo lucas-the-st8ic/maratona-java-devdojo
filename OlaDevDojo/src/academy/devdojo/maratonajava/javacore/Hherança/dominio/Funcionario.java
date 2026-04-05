3653a6c990c6c4db1d7eff56e5dc6f449e30d3f8
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-04-05 17:05:01 -0300

    updates

---
+++ OlaDevDojo/src/academy/devdojo/maratonajava/javacore/Hherança/dominio/Funcionario.java
@@ -0,0 +1,16 @@
+package academy.devdojo.maratonajava.javacore.Hherança.dominio;
+
+public class Funcionario extends Pessoa {
+
+    private double salario;
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+
+
+}
---
+++ OlaDevDojo/src/academy/devdojo/maratonajava/javacore/Hherança/test/HerancaTest.java
@@ -0,0 +1,34 @@
+package academy.devdojo.maratonajava.javacore.Hherança.test;
+
+import academy.devdojo.maratonajava.javacore.Hherança.dominio.Endereco;
+import academy.devdojo.maratonajava.javacore.Hherança.dominio.Funcionario;
+import academy.devdojo.maratonajava.javacore.Hherança.dominio.Pessoa;
+
+public class HerancaTest {
+    static void main(String[] args) {
+        Endereco endereco = new Endereco();
+        Endereco enderecoBob = new Endereco();
+
+        endereco.setRua("Rua das moréias escandinavas.");
+        endereco.setCep("48925-684");
+
+        enderecoBob.setRua("Rua da Concha N°124");
+        enderecoBob.setCep("28638-387");
+
+        Pessoa pessoa1 = new Pessoa();
+        pessoa1.setNome("King Crab");
+        pessoa1.setCpf("62082619028");
+        pessoa1.setEndereco(endereco);
+
+        pessoa1.imprime();
+
+        Funcionario funcionario1 = new Funcionario();
+
+        funcionario1.setNome("Bob Esponja");
+        funcionario1.setCpf("54678868860");
+        funcionario1.setEndereco(enderecoBob);
+        funcionario1.setSalario(200);
+
+        funcionario1.imprime();
+    }
+}
