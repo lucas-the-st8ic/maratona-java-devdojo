c258a9839d97a8c3725ea959ce210439076bc868
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-04-08 18:46:34 -0300

    updates

--- OlaDevDojo/src/academy/devdojo/maratonajava/javacore/Jmodificadorfinal/dominio/Comprador.java
+++ OlaDevDojo/src/academy/devdojo/maratonajava/javacore/Jmodificadorfinal/dominio/Comprador.java
@@ -1,11 +1,8 @@
 package academy.devdojo.maratonajava.javacore.Jmodificadorfinal.dominio;
 
-public class Carro {
+public class Comprador {
 
     private String nome;
-    public static final double VELOCIDADE_LIMITE = 250;
-    public final Comprador COMPRADOR = new Comprador();
-
 
     public String getNome() {
         return nome;
@@ -14,5 +11,11 @@ public class Carro {
     public void setNome(String nome) {
         this.nome = nome;
     }
-    
+
+    @Override
+    public String toString() {
+        return "Comprador{" +
+                "nome='" + nome + '\'' +
+                '}';
+    }
 }
