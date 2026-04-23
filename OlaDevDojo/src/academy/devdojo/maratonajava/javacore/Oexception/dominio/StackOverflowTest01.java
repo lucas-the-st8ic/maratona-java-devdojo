9a46740f6936c22450a45bd9a119ad809d3dd68b
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-04-23 15:28:06 -0300

    updates

---
+++ OlaDevDojo/src/academy/devdojo/maratonajava/javacore/Oexception/dominio/StackOverflowTest01.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.javacore.Oexception.dominio;
+
+public class StackOverflowTest01 {
+    static void main(String[] args) {
+
+    }
+
+    public static void recursividade() {
+        recursividade();
+    }
+}
