a23be0382815cc49c8652bc78b23208b65159c54
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-04-28 17:58:24 -0300

    updates

--- OlaDevDojo/src/academy/devdojo/maratonajava/javacore/Oexception/runtime/test/RuntimeExceptionTest01.java
+++ OlaDevDojo/src/academy/devdojo/maratonajava/javacore/Oexception/runtime/test/RuntimeExceptionTest01.java
@@ -1,6 +1,6 @@
 package academy.devdojo.maratonajava.javacore.Oexception.runtime.test;
 
-public class RunTimeExceptionTest01 {
+public class RuntimeExceptionTest01 {
     static void main(String[] args) {
         /*Checked e Unchecked*/
 
---
+++ OlaDevDojo/src/academy/devdojo/maratonajava/javacore/Oexception/runtime/test/RuntimeExceptionTest02.java
@@ -0,0 +1,20 @@
+package academy.devdojo.maratonajava.javacore.Oexception.runtime.test;
+
+public class RuntimeExceptionTest02 {
+    static void main(String[] args) {
+        divisao(1, 0);
+    }
+
+    /**
+     * @param a
+     * @param b não pode ser zero
+     * @return
+     * @throws IllegalArgumentException caso b seja zero*/
+    private static int divisao(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Argumento ilegal, divisor" +
+                    " não pode ser 0.");
+        }
+       return a / b;
+    }
+}
