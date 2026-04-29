ca4aa46c84ab08e86188b680cf4c685ec693b3cc
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-04-29 15:37:07 -0300

    Create ExceptionTest02.java

---
+++ OlaDevDojo/src/academy/devdojo/maratonajava/javacore/Oexception/exception/test/ExceptionTest02.java
@@ -0,0 +1,29 @@
+package academy.devdojo.maratonajava.javacore.Oexception.exception.test;
+
+import java.io.File;
+import java.io.IOException;
+
+public class ExceptionTest02 {
+    static void main(String[] args) throws IOException {
+        criarNovoArquivo();
+
+    }
+
+    private static void criarNovoArquivo() throws IOException {
+        File file = new File("arquivo\\teste.txt");
+
+        try {
+        boolean isCriado = file.createNewFile();
+        System.out.println("Arquivo Criado: " +isCriado);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw e;
+        }
+
+    }
+
+
+}
+
+
