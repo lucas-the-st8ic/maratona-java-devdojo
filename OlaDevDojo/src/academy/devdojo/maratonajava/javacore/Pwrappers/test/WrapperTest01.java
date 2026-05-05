30eb3d39111c4794a08340b9bb30ced33ecf8508
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-05-05 15:50:21 -0300

    Create WrapperTest01.java

---
+++ OlaDevDojo/src/academy/devdojo/maratonajava/javacore/Pwrappers/test/WrapperTest01.java
@@ -0,0 +1,26 @@
+package academy.devdojo.maratonajava.javacore.Pwrappers.test;
+
+public class WrapperTest01 {
+    static void main(String[] args) {
+    /*Wrappers são objetos
+    que irão encapsular tipos primitivos
+    */
+
+        byte byteP = 1;
+        short shortP = 1;
+        int intP = 1;
+        long longP = 10L;
+        float floatP = 10F;
+        char charp = 'W';
+        boolean booleanP = false;
+
+        byte byteW = 1;
+        short shortW = 1;
+        int intW = 1;
+        long longW = 10L;
+        float floatW = 10F;
+        char charW = 'W';
+        boolean booleanW = false;
+    }
+
+}
