d9808a2c5ca59edac3641ffbc824277f1a8f9421
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-05-11 17:39:07 -0300

    updates

--- OlaDevDojo/src/academy/devdojo/maratonajava/javacore/Qstring/test/StringTest02.java
+++ OlaDevDojo/src/academy/devdojo/maratonajava/javacore/Qstring/test/StringTest02.java
@@ -3,11 +3,15 @@ package academy.devdojo.maratonajava.javacore.Qstring.test;
 public class StringTest02 {
     static void main(String[] args) {
         String nome = "Lucas";
-
+        String numeros = "0123456";
         System.out.println(nome.charAt(0));
         System.out.println(nome.length());
         System.out.println(nome.replace("L","l"));
+        System.out.println(nome.toLowerCase());
+        System.out.println(nome.toUpperCase());
 
-        System.out.println(nome);
+        System.out.println(numeros.length());
+        System.out.println(numeros.substring(0,2));
+        System.out.println(nome.trim());
     }
 }
