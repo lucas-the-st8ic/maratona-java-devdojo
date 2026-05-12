718c9446394c841c08d1e2149e4270451c3b15a7
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-05-12 16:59:24 -0300

    updates

---
+++ OlaDevDojo/src/academy/devdojo/maratonajava/javacore/Qstring/test/StringBuilderTest01.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava.javacore.Qstring.test;
+
+public class StringBuilderTest01 {
+    static void main(String[] args) {
+        String nome = "Lucas Silva";
+        nome.concat(" Dev Java");
+        nome.substring(0, 3);
+        System.out.println(nome);
+        System.out.println("===============");
+
+        StringBuilder stringBuilder = new StringBuilder("Lucas Silva");
+        stringBuilder.append(" Dev Java").append(" backend");
+        String substring = stringBuilder.substring(0, 2);
+        System.out.println(stringBuilder);
+
+    }
+}
