7416be7ced01ba3f32e6e368ff09bdfdb05cd715
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-05-14 12:55:55 -0300

    updates

--- OlaDevDojo/src/academy/devdojo/maratonajava/javacore/Sformatacao/test/LocaleTest02.java
+++ OlaDevDojo/src/academy/devdojo/maratonajava/javacore/Sformatacao/test/LocaleTest02.java
@@ -1,4 +1,21 @@
 package academy.devdojo.maratonajava.javacore.Sformatacao.test;
 
+import java.util.Locale;
+
 public class LocaleTest02 {
+    static void main(String[] args) {
+        System.out.println(Locale.getDefault());
+        String[] isoCountriesList = Locale.getISOCountries();
+        String[] isoLanguagesList = Locale.getISOLanguages();
+        
+        for(String isoCountrie : isoCountriesList) {
+            System.out.print(isoCountrie + " ");
+        }
+
+        System.out.println();
+
+        for(String isoLanguage : isoLanguagesList) {
+            System.out.print(isoLanguage + " ");
+        }
+    }
 }
