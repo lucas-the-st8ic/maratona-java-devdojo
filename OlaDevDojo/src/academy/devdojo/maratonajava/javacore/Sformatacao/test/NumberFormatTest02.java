53e71d955ac87df2185a2f709ba93e7919caf54f
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-05-17 16:25:48 -0300

    updates

--- OlaDevDojo/src/academy/devdojo/maratonajava/javacore/Sformatacao/test/NumberFormatTest02.java
+++ OlaDevDojo/src/academy/devdojo/maratonajava/javacore/Sformatacao/test/NumberFormatTest02.java
@@ -4,8 +4,8 @@ import java.text.NumberFormat;
 import java.text.ParseException;
 import java.util.Locale;
 
-public class NumberFormatTest01 {
-    static void main(String[] args)  {
+public class NumberFormatTest02 {
+    static void main(String[] args) {
 
         Locale localeDefault = Locale.getDefault();
         Locale localeBr = new Locale("pt", "BR");
@@ -14,26 +14,26 @@ public class NumberFormatTest01 {
 
         NumberFormat[] numberFormats = new NumberFormat[4];
 
-        numberFormats[0] = NumberFormat.getInstance();
-        numberFormats[1] = NumberFormat.getInstance(localeBr);
-        numberFormats[2] = NumberFormat.getInstance(localeJp);
-        numberFormats[3] = NumberFormat.getInstance(localeIt);
+        numberFormats[0] = NumberFormat.getCurrencyInstance();
+        numberFormats[1] = NumberFormat.getCurrencyInstance(localeBr);
+        numberFormats[2] = NumberFormat.getCurrencyInstance(localeJp);
+        numberFormats[3] = NumberFormat.getCurrencyInstance(localeIt);
 
-        double valor = 1_000.2130;
+        double valor = 1000.2130;
         
         for(NumberFormat numbers : numberFormats ) {
             System.out.println(numbers.getMaximumFractionDigits());
-            numbers.setMaximumFractionDigits(2);
             System.out.println(numbers.format(valor));
-            System.out.println();
         }
+        String valorString = "￥1,000";
 
-        String valorString = "1000.2130";
 
         try {
-            System.out.println(numberFormats[0].parse(valorString));;
+            System.out.println(numberFormats[2].parse(valorString));;
         } catch (ParseException e) {
             e.printStackTrace();
         }
+
+
     }
 }
