65157190540e5b46347a7b9b9e0981147ae45666
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-05-19 18:01:20 -0300

    updates

---
+++ OlaDevDojo/src/academy/devdojo/maratonajava/javacore/Rdatas/test/LocalTimeTest01.java
@@ -0,0 +1,21 @@
+package academy.devdojo.maratonajava.javacore.Rdatas.test;
+
+import java.time.LocalTime;
+import java.time.temporal.ChronoField;
+
+public class LocalTimeTest01 {
+    static void main(String[] args) {
+        LocalTime localTime = LocalTime.of(23, 32, 12);
+        LocalTime localTimeNow = LocalTime.now();
+
+        System.out.println(localTime);
+        System.out.println(localTimeNow);
+        System.out.println(localTime.getHour());
+        System.out.println(localTime.getMinute());
+        System.out.println(localTime.get(ChronoField.CLOCK_HOUR_OF_DAY));
+        System.out.println(localTime.MIN);
+        System.out.println(localTime.MAX);
+        
+
+    }
+}
