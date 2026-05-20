db512bef6b23d33d51e4773a49ced77ca958626f
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-05-20 12:52:44 -0300

    updates

--- OlaDevDojo/src/academy/devdojo/maratonajava/javacore/Rdatas/test/DurationTest01.java
+++ OlaDevDojo/src/academy/devdojo/maratonajava/javacore/Rdatas/test/DurationTest01.java
@@ -1,4 +1,23 @@
 package academy.devdojo.maratonajava.javacore.Rdatas.test;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
 public class DurationTest01 {
+    static void main(String[] args) {
+
+        LocalDateTime now = LocalDateTime.now();
+
+        LocalDateTime birthday = LocalDateTime.of(2026, 06,10, 10, 36);
+
+        LocalTime timeNow = LocalTime.now();
+        LocalTime timeMinus7Hours = LocalTime.now().minusHours(7);
+
+
+        Duration duration = Duration.between(now, birthday);
+        Duration durationMinus7Hours = Duration.between(timeNow, timeMinus7Hours);
+        System.out.println(duration);
+        System.out.println(durationMinus7Hours);
+    }
 }
