5cdf4364e1cbbf6fe3a6c4c37bfebe84f2708115
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-05-22 15:51:33 -0300

    Update Ex01.java

--- OlaDevDojo/src/academy/devdojo/maratonajava/exercicios/exercicios_java/modulo02/Ex01.java
+++ OlaDevDojo/src/academy/devdojo/maratonajava/exercicios/exercicios_java/modulo02/Ex01.java
@@ -6,12 +6,25 @@ public class Ex01 {
         // tipos primitivos e imprimir seus valores padrão
 
         String name = null;
+        byte numByte = 127;
+        short numShort = 32767;
         int numInt = 0;
+        long numLong = 1214353543539368356l;
         double numDouble = 0;
-        
+        float numFloat = 10.5f;
+        char character = 'a';
+        boolean bool = true;
+
+
 
         System.out.println(name);
+        System.out.println(numByte);
+        System.out.println(numShort);
         System.out.println(numInt);
+        System.out.println(numLong);
         System.out.println(numDouble);
+        System.out.println(numFloat);
+        System.out.println(character);
+        System.out.println(bool);
     }
 }
