e00216d302ffda23faee1154793c5671a3a94641
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-05-22 15:27:06 -0300

    updates

---
+++ OlaDevDojo/src/academy/devdojo/maratonajava/exercicios/exercicios_java/modulo02/Ex01.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava.exercicios.exercicios_java.modulo02;
+
+public class Ex01 {
+    static void main(String[] args) {
+        //1. Declarar variáveis de todos os 8
+        // tipos primitivos e imprimir seus valores padrão
+
+        String name = null;
+        int numInt = 0;
+        double numDouble = 0;
+        
+
+        System.out.println(name);
+        System.out.println(numInt);
+        System.out.println(numDouble);
+    }
+}
