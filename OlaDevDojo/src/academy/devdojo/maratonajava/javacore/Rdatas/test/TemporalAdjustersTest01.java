d1bb61db40691e3cd6931b29dbd0b5afb9092f72
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-05-23 18:09:33 -0300

    updates

--- OlaDevDojo/src/academy/devdojo/maratonajava/javacore/Rdatas/test/TemporalAdjustersTest01.java
+++ OlaDevDojo/src/academy/devdojo/maratonajava/javacore/Rdatas/test/TemporalAdjustersTest01.java
@@ -1,7 +1,32 @@
 package academy.devdojo.maratonajava.javacore.Rdatas.test;
 
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.temporal.ChronoField;
+import java.time.temporal.TemporalAdjusters;
+
 public class TemporalAdjustersTest01 {
     static void main(String[] args) {
-        
+        LocalDate now = LocalDate.now();
+        now = now.with(ChronoField.DAY_OF_MONTH, 23);
+        System.out.println(now);
+        System.out.println(now.getDayOfWeek());
+
+        now = LocalDate.now().with(TemporalAdjusters.nextOrSame(DayOfWeek.MONDAY));
+        System.out.println(now);
+        System.out.println(now.getDayOfWeek());
+
+        now = LocalDate.now().with(TemporalAdjusters.previous(DayOfWeek.MONDAY));
+        System.out.println(now);
+        System.out.println(now.getDayOfWeek());
+
+        now = LocalDate.now().with(TemporalAdjusters.firstDayOfMonth());
+        System.out.println(now);
+        System.out.println(now.getDayOfWeek());
+
+        now = LocalDate.now().with(TemporalAdjusters.lastDayOfMonth());
+        System.out.println(now);
+        System.out.println(now.getDayOfWeek());
+
     }
 }
