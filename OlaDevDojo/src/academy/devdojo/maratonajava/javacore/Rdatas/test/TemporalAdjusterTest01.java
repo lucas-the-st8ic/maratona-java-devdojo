f82a442be5500041c9de3d06d0f6827ab2990889
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-05-23 19:25:35 -0300

    updates

---
+++ OlaDevDojo/src/academy/devdojo/maratonajava/javacore/Rdatas/test/TemporalAdjusterTest01.java
@@ -0,0 +1,61 @@
+package academy.devdojo.maratonajava.javacore.Rdatas.test;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.temporal.ChronoField;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.Temporal;
+import java.time.temporal.TemporalAdjuster;
+
+public class TemporalAdjusterTest01 {
+    static void main(String[] args) {
+        LocalDate now = LocalDate.now();
+
+        System.out.println(now);
+        System.out.println(now.getDayOfWeek());
+
+        now = LocalDate.now().with(new ObterProximoDiaUtil());
+
+        System.out.println(now);
+        System.out.println(now.getDayOfWeek());
+
+        now = LocalDate.now().withDayOfMonth(29).with(new ObterProximoDiaUtil());
+
+        System.out.println(now);
+        System.out.println(now.getDayOfWeek());
+    }
+
+
+    static class ObterProximoDiaUtil implements TemporalAdjuster {
+
+        @Override
+        public Temporal adjustInto(Temporal temporal) {
+
+            DayOfWeek dayOfWeek = DayOfWeek.of(temporal.get(ChronoField.DAY_OF_WEEK));
+
+            int addDays = 1;
+
+            switch (dayOfWeek) {
+                case THURSDAY:
+                    addDays = 4;
+                    break;
+
+                case FRIDAY:
+                    addDays = 3;
+                    break;
+
+                case SATURDAY:
+                    addDays = 2;
+                    break;
+
+                case SUNDAY:
+                    addDays = 1;
+                    break;
+
+                default:
+                    addDays = 1;
+            }
+            return temporal.plus(addDays, ChronoUnit.DAYS);
+        }
+    }
+}
