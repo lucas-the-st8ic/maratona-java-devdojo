86a58773bd5a09e75fa0cecbfc35ba08d02b9b0f
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-06-25 14:06:27 -0300

    Updates

--- OlaDevDojo/src/academy/devdojo/maratonajava/javacore/Wnio/test/PathMatcherTest02.java
+++ OlaDevDojo/src/academy/devdojo/maratonajava/javacore/Wnio/test/PathMatcherTest02.java
@@ -5,16 +5,20 @@ import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
 
 class findAllTestJavaOrClass extends SimpleFileVisitor<Path> {
-    private PathMatcher matcher = FileSystems.getDefault().getPathMatcher("");
+    private PathMatcher matcher = FileSystems.getDefault().getPathMatcher("glob:**/*{Test*}.{java,class}");
 
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-        return super.visitFile(file, attrs);
+        if (matcher.matches(file)) {
+            System.out.println("File found: " + file.getFileName());
+        }
+        return FileVisitResult.CONTINUE;
     }
 }
 
 public class PathMatcherTest02 {
-    static void main(String[] args) {
-
+    static void main(String[] args) throws IOException {
+        Path root = Paths.get(".");
+        Files.walkFileTree(root, new findAllTestJavaOrClass());
     }
 }
