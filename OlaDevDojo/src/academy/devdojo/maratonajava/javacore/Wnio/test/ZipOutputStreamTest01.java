b66ca8fd91a363dfc70510367817db08d307d80d
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-06-29 18:56:36 -0300

    updates

---
+++ OlaDevDojo/src/academy/devdojo/maratonajava/javacore/Wnio/test/ZipOutputStreamTest01.java
@@ -0,0 +1,22 @@
+package academy.devdojo.maratonajava.javacore.Wnio.test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.zip.ZipOutputStream;
+
+public class ZipOutputStreamTest01 {
+    static void main(String[] args) {
+        Path arquivoZip = Paths.get("pasta/arquivo.zip");
+        Path arquivosParaZipar = Paths.get("pasta/subpasta1/subsubpasta1");
+    }
+
+    private static void zip(Path arquivoZip, Path arquivosParaZipar) {
+        try(ZipOutputStream zip = new ZipOutputStream(Files.newOutputStream(arquivoZip))) {
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
