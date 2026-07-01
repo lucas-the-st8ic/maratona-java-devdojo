336524dc128ed69ff85ede589e7a537395d5b113
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-07-01 14:05:39 -0300

    Updates

--- OlaDevDojo/src/academy/devdojo/maratonajava/javacore/Wnio/test/ZipOutputStreamTest01.java
+++ OlaDevDojo/src/academy/devdojo/maratonajava/javacore/Wnio/test/ZipOutputStreamTest01.java
@@ -5,6 +5,7 @@ import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 public class ZipOutputStreamTest01 {
@@ -19,8 +20,12 @@ public class ZipOutputStreamTest01 {
             DirectoryStream<Path> directoryStream = Files.newDirectoryStream(arquivosParaZipar);)
         {
             for (Path file : directoryStream) {
-                System.out.println(file.getFileName());
+                ZipEntry zipEntry = new ZipEntry(file.getFileName().toString());
+                zip.putNextEntry(zipEntry);
+                Files.copy(file, zip);
+                zip.closeEntry();
             }
+            System.out.println("Arquivo criado com sucesso");
         } catch (IOException e) {
             e.printStackTrace();
         }
