20e2999a5387baaf7fe7341b1d2a01164704c092
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-07-01 12:44:30 -0300

    Updates

--- OlaDevDojo/src/academy/devdojo/maratonajava/javacore/Wnio/test/ZipOutputStreamTest01.java
+++ OlaDevDojo/src/academy/devdojo/maratonajava/javacore/Wnio/test/ZipOutputStreamTest01.java
@@ -1,6 +1,7 @@
 package academy.devdojo.maratonajava.javacore.Wnio.test;
 
 import java.io.IOException;
+import java.nio.file.DirectoryStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -10,11 +11,16 @@ public class ZipOutputStreamTest01 {
     static void main(String[] args) {
         Path arquivoZip = Paths.get("pasta/arquivo.zip");
         Path arquivosParaZipar = Paths.get("pasta/subpasta1/subsubpasta1");
+        zip(arquivoZip, arquivosParaZipar);
     }
 
     private static void zip(Path arquivoZip, Path arquivosParaZipar) {
-        try(ZipOutputStream zip = new ZipOutputStream(Files.newOutputStream(arquivoZip))) {
-
+        try(ZipOutputStream zip = new ZipOutputStream(Files.newOutputStream(arquivoZip));
+            DirectoryStream<Path> directoryStream = Files.newDirectoryStream(arquivosParaZipar);)
+        {
+            for (Path file : directoryStream) {
+                System.out.println(file.getFileName());
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
