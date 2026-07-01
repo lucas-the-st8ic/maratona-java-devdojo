2b83af289783d46a0fd9aac5a6f1639cf773ba08
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-07-01 16:47:55 -0300

    Updates

---
+++ OlaDevDojo/src/academy/devdojo/maratonajava/javacore/Xserializacao/test/SerializacaoTest01.java
@@ -0,0 +1,39 @@
+package academy.devdojo.maratonajava.javacore.Xserializacao.test;
+
+import academy.devdojo.maratonajava.javacore.Xserializacao.dominio.Aluno;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class SerializacaoTest01 {
+    static void main(String[] args) {
+        Aluno aluno01 = new Aluno(010102L, "Bob Esponja", "934875845");
+//           serializar(aluno01);
+            deserializar();
+    }
+
+    private static void serializar(Aluno aluno) {
+        Path path = Paths.get("pasta/aluno.ser");
+        try(ObjectOutputStream obj = new ObjectOutputStream(
+                Files.newOutputStream(path))) {
+            obj.writeObject(aluno);
+        } catch (IOException e) {
+            e.printStackTrace();
+        };
+    }
+
+    private static void deserializar() {
+        Path path = Paths.get("pasta/aluno.ser");
+        try(ObjectInputStream objInpt = new ObjectInputStream(
+                Files.newInputStream(path))) {
+            Aluno aluno = (Aluno) objInpt.readObject();
+            System.out.println(aluno);
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        };
+    }
+}
