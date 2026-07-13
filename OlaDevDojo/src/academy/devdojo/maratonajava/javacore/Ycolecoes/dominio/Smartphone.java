20aba818fe2931771dd3c577df1af103bec5fe37
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-07-13 12:56:29 -0300

    Update Smartphone.java

--- OlaDevDojo/src/academy/devdojo/maratonajava/javacore/Ycolecoes/dominio/Smartphone.java
+++ OlaDevDojo/src/academy/devdojo/maratonajava/javacore/Ycolecoes/dominio/Smartphone.java
@@ -35,6 +35,18 @@ public class Smartphone {
                 serialNumber.equals(smartphone.serialNumber);
     }
 
+    /*
+    - Se x.equals(y) == true, y.hashCode() == x.hashCode()
+    - y.hashCode() == x.hashCode() não necessariamente o equals de y.equals(x)
+    tem que ser true
+    - x.equals(y) == false
+    y.hashCode() != x.hashCode() x.equals(y) deverá ser false.
+    */
+    @Override
+    public int hashCode() {
+        return serialNumber == null ? 0 : this.serialNumber.hashCode();
+    }
+
     public String getSerialNumber() {
         return serialNumber;
     }
