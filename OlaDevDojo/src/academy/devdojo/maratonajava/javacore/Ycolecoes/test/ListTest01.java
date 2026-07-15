58d9ea2b99d859f2a9c6469f0d36ba35dad44544
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-07-15 11:27:14 -0300

    Updates

--- OlaDevDojo/src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/ListTest01.java
+++ OlaDevDojo/src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/ListTest01.java
@@ -6,17 +6,33 @@ import java.util.List;
 public class ListTest01 {
     static void main(String[] args) {
         List<String> nomes = new ArrayList<>();//Até a versão 1.4 Java
+        List<String> nomes2 = new ArrayList<>();
         nomes.add("Bob Esponja");
         nomes.add("Lula Molusco");
         nomes.add("Patrick");
+        //nomes.remove("Patrick");
 
+
+        nomes2.add("Lucas");
+        nomes2.add("DevDojo");
+        nomes2.add("DevDojo academy");
+
+        nomes.addAll(nomes2);
         for(String nome : nomes) {
             System.out.println(nome);
         }
+
         nomes.add("Sirigueijo");
         System.out.println("-----------");
         for (int i = 0; i < nomes.size(); i++) {
             System.out.println(nomes.get(i));
+
         }
+
+        List<Integer> numeros = new ArrayList<>();
+        numeros.add(1);
+
+        int num = 9119;
+        System.out.println(9119 % 10);
     }
 }
