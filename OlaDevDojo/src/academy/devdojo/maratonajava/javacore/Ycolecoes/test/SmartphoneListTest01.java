507858330df823c7d84a43a84242c1a8143a0f0d
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-07-15 13:52:03 -0300

    Create SmartphoneListTest01.java

---
+++ OlaDevDojo/src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/SmartphoneListTest01.java
@@ -0,0 +1,11 @@
+package academy.devdojo.maratonajava.javacore.Ycolecoes.test;
+
+import academy.devdojo.maratonajava.javacore.Ycolecoes.dominio.Smartphone;
+
+public class SmartphoneListTest01 {
+    static void main(String[] args) {
+        Smartphone cel1 = new Smartphone("1ABC1", "Iphone 17");
+        Smartphone cel2 = new Smartphone("2ABC1", "Motorola Edge 60");
+        Smartphone cel3 = new Smartphone("2ABCD1","Samsung Galaxy S9");
+    }
+}
