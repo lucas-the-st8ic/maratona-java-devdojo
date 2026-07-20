b50261a7cd30c98de94c8739290b744e3201c61e
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-07-20 18:58:24 -0300

    Create BinarySearchTest01.java

---
+++ OlaDevDojo/src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/BinarySearchTest01.java
@@ -0,0 +1,9 @@
+package academy.devdojo.maratonajava.javacore.Ycolecoes.test;
+
+import java.util.List;
+
+public class BinarySearchTest01 {
+    static void main(String[] args) {
+        
+    }
+}
