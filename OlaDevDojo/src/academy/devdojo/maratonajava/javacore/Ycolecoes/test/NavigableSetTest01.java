5ef539bafbe21009642f6eff651e22330f06015f
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-07-24 15:56:46 -0300

    Create NavigableSetTest01.java

---
+++ OlaDevDojo/src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/NavigableSetTest01.java
@@ -0,0 +1,9 @@
+package academy.devdojo.maratonajava.javacore.Ycolecoes.test;
+
+import java.util.NavigableSet;
+
+public class NavigableSetTest01 {
+    static void main(String[] args) {
+        NavigableSet
+    }
+}
