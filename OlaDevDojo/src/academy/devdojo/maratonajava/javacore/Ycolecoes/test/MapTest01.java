9236667244fcc84f0784d4a46065ff69190cd93c
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-07-26 16:46:57 -0300

    Updates

--- OlaDevDojo/src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/MapTest01.java
+++ OlaDevDojo/src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/MapTest01.java
@@ -1,17 +1,30 @@
 package academy.devdojo.maratonajava.javacore.Ycolecoes.test;
 
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class MapTest01 {
     static void main(String[] args) {
-         Map<String, String> map = new HashMap<>();
-         map.put("teklado", "teclado");
-         map.put("mouze", "mouse");
-         map.put("vc", "você");
-         map.putIfAbsent("vc2", "você");
+        Map<String, String> map = new LinkedHashMap<>();
+        map.put("teklado", "teclado");
+        map.put("mouze", "mouse");
+        map.put("vc", "você");
+        map.putIfAbsent("vc2", "você");
         System.out.println(map);
 
-        
+        for (String key : map.keySet()) {
+            System.out.println("(Chave)" + key + ": " + "(Valor)" + map.get(key));
+        }
+        System.out.println("====================");
+
+        for (String value : map.values()) {
+            System.out.println(value);
+        }
+        System.out.println("====================");
+
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            System.out.println(entry.getKey()+" - "+entry.getValue());
+        }
     }
 }
---
+++ OlaDevDojo/src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/MapTest02.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.javacore.Ycolecoes.test;
+
+import academy.devdojo.maratonajava.javacore.Ycolecoes.dominio.Consumidor;
+
+public class MapTest02 {
+    static void main(String[] args) {
+        Consumidor consumidor1 = new Consumidor("Lucas");
+        System.out.println(consumidor1);
+    }
+}
