7efd37be0a34bad8e2a097ff4ed6f2051b691687
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-07-26 16:11:08 -0300

    Updates

--- OlaDevDojo/src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/MapTest01.java
+++ OlaDevDojo/src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/MapTest01.java
@@ -1,9 +1,17 @@
 package academy.devdojo.maratonajava.javacore.Ycolecoes.test;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class MapTest01 {
     static void main(String[] args) {
-        Map
+         Map<String, String> map = new HashMap<>();
+         map.put("teklado", "teclado");
+         map.put("mouze", "mouse");
+         map.put("vc", "você");
+         map.putIfAbsent("vc2", "você");
+        System.out.println(map);
+
+        
     }
 }
