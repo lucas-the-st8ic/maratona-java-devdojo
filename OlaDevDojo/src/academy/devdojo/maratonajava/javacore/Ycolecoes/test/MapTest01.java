0c85a46b65b70568a5cfb57082bbc88ec56bfd4b
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-07-26 15:04:01 -0300

    Update MapTest01.java

--- OlaDevDojo/src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/MapTest01.java
+++ OlaDevDojo/src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/MapTest01.java
@@ -1,7 +1,9 @@
 package academy.devdojo.maratonajava.javacore.Ycolecoes.test;
 
+import java.util.Map;
+
 public class MapTest01 {
     static void main(String[] args) {
-        list
+        Map
     }
 }
