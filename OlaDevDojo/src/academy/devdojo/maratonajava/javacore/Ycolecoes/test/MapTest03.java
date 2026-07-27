46d54cc8da098268ca1cb3021aec7f20a460131d
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-07-27 16:27:35 -0300

    Updates

--- OlaDevDojo/src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/MapTest03.java
+++ OlaDevDojo/src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/MapTest03.java
@@ -3,10 +3,12 @@ package academy.devdojo.maratonajava.javacore.Ycolecoes.test;
 import academy.devdojo.maratonajava.javacore.Ycolecoes.dominio.Consumidor;
 import academy.devdojo.maratonajava.javacore.Ycolecoes.dominio.Manga;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
-public class MapTest02 {
+public class MapTest03 {
     static void main(String[] args) {
         Consumidor consumidor1 = new Consumidor("Lucas");
         Consumidor consumidor2 = new Consumidor("DevDojo Academy");
@@ -19,13 +21,20 @@ public class MapTest02 {
         Manga hajimeNoIppo = new Manga(2L, "Hajime No Ippo", 22.0, 892);
         Manga hunterXHunter = new Manga(34L, "HunterXHunter", 19.99, 743);
 
-        Map<Consumidor, Manga> consumidorManga = new HashMap<>();
+        List<Manga> mangaConsumidor1List = List.of(berserk, pokemon, attackOnTitan);
+        List<Manga> mangaConsumidor2List = List.of(attackOnTitan, vagabond, hunterXHunter);
+        Map<Consumidor, List<Manga>> consumidorMangaMap = new HashMap<>();
 
-        consumidorManga.put(consumidor1, fullmetalAlchemist);
-        consumidorManga.put(consumidor2, hunterXHunter);
+        consumidorMangaMap.put(consumidor1, mangaConsumidor1List);
+        consumidorMangaMap.put(consumidor2, mangaConsumidor2List);
 
-        for(Map.Entry<Consumidor, Manga> entry : consumidorManga.entrySet()){
-            System.out.println(entry.getKey().getNome()+ ": " +entry.getValue().getNome());
+        for (Map.Entry<Consumidor, List<Manga>> entry :
+        consumidorMangaMap.entrySet()) {
+            System.out.println(entry.getKey().getNome());
+            for (Manga manga : entry.getValue()) {
+                System.out.println(manga.getNome());
+            }
+            System.out.println("===============================");
         }
 
     }
