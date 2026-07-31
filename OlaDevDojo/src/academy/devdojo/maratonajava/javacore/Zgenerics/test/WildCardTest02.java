539a97fea1ba743a7382d441c46d63ff3be1c263
Lucas Silva <lucas.silva.s1998@gmail.com>
2026-07-31 17:39:54 -0300

    Update WildCardTest02.java

--- OlaDevDojo/src/academy/devdojo/maratonajava/javacore/Zgenerics/test/WildCardTest02.java
+++ OlaDevDojo/src/academy/devdojo/maratonajava/javacore/Zgenerics/test/WildCardTest02.java
@@ -8,8 +8,8 @@ public class WildCardTest02 {
         List<Cachorro> cachorros = List.of(new Cachorro(), new Cachorro());
         List<Gato> gatos = List.of(new Gato(), new Gato());
 
-        printConsulta(cachorros);
-        printConsulta(gatos);
+        /*printConsulta(cachorros);
+        printConsulta(gatos);*/
     }
 
     private static void printConsulta(List<Animal> animals){
